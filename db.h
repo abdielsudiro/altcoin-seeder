4e84cce22ed1a446fe42701d322789c434aa5775
Alex Shepherd <alex@jaybox.co.uk>
2014-05-31 03:08:05 +0100

    Changes for XC

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 230000;
+    return 0; // No forks yet
 }
 
 std::string static inline ToString(const CService &ip) {
